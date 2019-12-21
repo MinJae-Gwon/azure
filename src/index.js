d26bfbf0a0d5e7fc82ac42b2887d4ed1402bd48b
MinJaeGwon <minjae2271@gmail.com>
2019-12-21 20:53:29 +0900

    pwa01

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => (
+  <div className="app">
+    Hi
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+    })
+}
