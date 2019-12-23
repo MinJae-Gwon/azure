2d059fa53b3dae0e815397446a54081550b0fe6b
MinJaeGwon <minjae2271@gmail.com>
2019-12-24 01:03:02 +0900

    SW

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest')
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
